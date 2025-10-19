510a20185231dc7eac7397c8db46ceb3ec736114
Max <mc.cui@mail.utoronto.ca>
2025-10-19 15:38:27 -0400

    created decoder and its custom lstm that accepts initial states

---
+++ models/decoder.py
@@ -0,0 +1,31 @@
+import mlx.core as mx
+import mlx.nn as nn
+from custom_lstm import CustomLSTM
+
+class SelfiesDecoder(nn.Module):
+    def __init__(
+        self,
+        vocab_size,
+        embedding_dim: int=128,
+        hidden_dim: int=256,
+        latent_dim: int=64,
+    ):
+        super().__init__()
+        self.latent_h = nn.Linear(latent_dim, hidden_dim)
+        self.latent_c = nn.Linear(latent_dim, hidden_dim)
+        self.embed = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = CustomLSTM(embedding_dim, hidden_dim)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+
+    def __call__(self, z, seq):
+        #z: [B, L]
+        #seq: [B, T]
+        init_h = mx.tanh(self.latent_h(z))  # [B, H]
+        init_c = mx.tanh(self.latent_c(z))  # [B, H]
+        x = self.embed(seq)  # [B, T, E]
+        
+        # Initialize LSTM with latent-derived hidden states
+        initial_state = (init_h, init_c)
+        outputs, _ = self.lstm(x, initial_state)
+        logits = self.fc(outputs)
+        return logits
